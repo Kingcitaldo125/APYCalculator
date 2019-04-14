bb16b6f523c228ae7bd0c5c5efca77967c531317
paula <paularelt@gmail.com>
2019-04-14 11:50:13 -0400

    Adding numerical limits and year limit to 100

--- InvestSense.py
+++ InvestSense.py
@@ -4,6 +4,8 @@
 import math
 import re
 
+totalLimit = 0xE8D4A51000
+
 def getRoundedString(numb):
 	retStr = str(numb)
 	reComp = re.match(r'(\d+)[.]\d*', str(numb))
@@ -16,7 +18,7 @@ def getRoundedString(numb):
 
 principal = int(input('Enter_Principal\n'))
 rate = int(input('Enter_Rate in Percent\n'))
-years = int(input('Enter Number of Years to cycle\n'))
+years = int(input('Enter Number of Years to cycle(no more than 100)\n'))
 
 
 rate = rate / 100
@@ -33,12 +35,17 @@ if years <= 0:
 	print("Rounding years to 1 year")
 	years = 1
 
+if years > 100:
+	print("Rounding years to 100")
+	years = 100
+
 
 total = round(principal, 2)
+print("Starting Principal:", getRoundedString(total))
 i = 0
 
 while i < years:
-	total += (total * rate)
+	total += (total * rate) % totalLimit
 	yearStr = str(i + 1)
 	
 	print(yearStr,"years total(rounded):", getRoundedString(total))
